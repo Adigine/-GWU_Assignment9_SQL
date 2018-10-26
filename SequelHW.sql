8b80383f2f1ad35a7684be2fc454eea01ac8496b
Adigine <41315484+Adigine@users.noreply.github.com>
2018-10-26 10:09:33 -0400

    Added sections 1, 2, and 6

--- SequelHW.sql
+++ SequelHW.sql
@@ -1,12 +1,27 @@
 USE sakila;
 
-# Part 1
+# Part 1____________________________________
 
+#1a.
+SELECT * FROM actor WHERE last_name = 'CHASE';
 
-# Part 2
+#1b
+ALTER TABLE actor
+ADD COLUMN Actor_Name VARCHAR(100);
+UPDATE actor SET Actor_Name = CONCAT(first_name, ' ', last_name);
 
+# Part 2____________________________________
 
-# Part 3a
+#2a. Inserting Blob Column
+ALTER TABLE actor
+ADD COLUMN description BLOB(100);
+
+#2b drop a column in actor
+ALTER TABLE actor DROP description;
+
+# Part 3____________________________________
+
+# 3a
 SELECT actor_id, first_name, last_name FROM actor
 WHERE first_name = "Joe";
 
@@ -24,16 +39,30 @@ SELECT country_id, country
 FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
-# Part 4
+# Part 4____________________________________
 
 
-# Part 5
+# Part 5____________________________________
 
 
-# Part 6
+# Part 6____________________________________
 
+#6a
+SELECT staff.first_name, staff.last_name, address.address
+FROM staff
+INNER JOIN address ON staff.address_id = address.address_id;
 
-# Part 7
+#6b
+
+#6c
+SELECT film_actor.film_id, film_actor.actor_id, film.title
+FROM film
+INNER JOIN film_actor ON film.film_id = film_actor.film_id;
+
+#6d
+Select * FROM film;
+
+# Part 7____________________________________
 
 # 7a
 SELECT title FROM film
@@ -44,14 +73,11 @@ WHERE title LIKE 'K%' OR 'Q%' IN
     );
     
 # 7b    
-SELECT * FROM customer;
+SELECT first_name, last_name, email FROM customer WHERE address_id IN
+	(SELECT address_id FROM address WHERE city_id IN
+		(SELECT city_id FROM city WHERE country_id IN
+			(SELECT country_id FROM country WHERE country = 'Canada')));
+
 # 7c
-SELECT title
-FROM film
-WHERE film_id IN
-(
-SELECT film_id
-FROM film_category
-WHERE category_id =  8
-);
-# Part 8
+
+# Part 8____________________________________
