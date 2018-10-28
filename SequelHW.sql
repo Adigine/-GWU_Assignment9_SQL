406b1f467fdda48eaede54ea21b945fb795d1937
Adigine <41315484+Adigine@users.noreply.github.com>
2018-10-27 23:03:47 -0400

    parts 1-6 complete

--- SequelHW.sql
+++ SequelHW.sql
@@ -1,49 +1,89 @@
 USE sakila;
+SET SQL_SAFE_UPDATES = 0;
 
 # Part 1____________________________________
 
 #1a.
-SELECT * FROM actor WHERE last_name = 'CHASE';
+SELECT * FROM actor;
+#SELECT * FROM actor WHERE last_name = 'CHASE';
 
 #1b
 ALTER TABLE actor
 ADD COLUMN Actor_Name VARCHAR(100);
 UPDATE actor SET Actor_Name = CONCAT(first_name, ' ', last_name);
+SELECT Actor_Name FROM actor;
 
 # Part 2____________________________________
 
-#2a. Inserting Blob Column
-ALTER TABLE actor
-ADD COLUMN description BLOB(100);
-
-#2b drop a column in actor
-ALTER TABLE actor DROP description;
-
-# Part 3____________________________________
-
-# 3a
+# 2a
 SELECT actor_id, first_name, last_name FROM actor
 WHERE first_name = "Joe";
 
-# 3b
+# 2b
 SELECT * FROM actor 
 WHERE last_name LIKE '%GEN%';
 
-# 3c
+# 2c
 SELECT * FROM actor 
 WHERE last_name LIKE '%LI%'
 ORDER BY last_name, first_name;
 
-#3d
+#2d
 SELECT country_id, country 
 FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
+
+# Part 3____________________________________
+
+#3a. Inserting Blob Column
+ALTER TABLE actor
+ADD COLUMN description BLOB(100);
+
+#3b drop a column in actor
+ALTER TABLE actor DROP description;
+
+
 # Part 4____________________________________
 
+# 4a
+Select last_name from actor;
+SELECT last_name, COUNT(*)
+    FROM actor
+    GROUP BY last_name
+    ORDER BY last_name;
+    
+# 4b
+SELECT last_name, COUNT(*)
+    FROM actor
+    GROUP BY last_name
+    HAVING COUNT(*) >= 2
+    ORDER BY last_name;
+
+
+# 4c 
+UPDATE actor
+SET first_name = 'HARPO'
+WHERE actor_id = 172;
+Select * from actor;
+
+# 4d
+UPDATE actor
+SET first_name = 'GROUCHO'
+WHERE actor_id = 172;
+Select * from actor;
 
 # Part 5____________________________________
 
+SHOW CREATE TABLE actor;
+CREATE TABLE `actor` (
+  `actor_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`actor_id`),
+  KEY `idx_actor_last_name` (`last_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8;
 
 # Part 6____________________________________
 
@@ -73,11 +113,39 @@ WHERE title LIKE 'K%' OR 'Q%' IN
     );
     
 # 7b    
+
+
+Select * from film;
+Select * from actor;
+Select * from film_actor;
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN
+(
+ SELECT actor_id
+ FROM film_actor
+ WHERE film_id = 17
+);
+
+# 7c
 SELECT first_name, last_name, email FROM customer WHERE address_id IN
 	(SELECT address_id FROM address WHERE city_id IN
 		(SELECT city_id FROM city WHERE country_id IN
 			(SELECT country_id FROM country WHERE country = 'Canada')));
-
-# 7c
+            
+#7d
+#sales lagging young families - target all family movies
+#all movies categorized as _family_films
+Select * from film;
+Select * from film_category;
+Select * from category;
+SELECT title
+FROM film
+WHERE film_id IN
+(
+ SELECT film_id
+ FROM film_category
+ WHERE category_id =  8
+);
 
 # Part 8____________________________________
